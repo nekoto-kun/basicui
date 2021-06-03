92d1c76eb1e9a3ebbe3b5ad75904646f51650fb2
Andre Kho <andreyonathan65@gmail.com>
2021-06-03 15:26:30 +0700

    Add visualDensity

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
         primarySwatch: yellowSwatch,
         brightness: Brightness.light,
         fontFamily: 'Montserrat',
