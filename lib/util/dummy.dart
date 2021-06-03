8bafacb020e03e5f0d2caf15c224861e0b3bf1d8
Andre Kho <andreyonathan65@gmail.com>
2021-06-03 15:27:17 +0700

    Add dummy data

---
+++ lib/util/dummy.dart
@@ -0,0 +1,38 @@
+const dummyData = [
+  {
+    'id': 1,
+    'title': 'Ordered Lele × 2',
+    'category': 'Food & Drinks',
+    'amount': 18000,
+  },
+  {
+    'id': 2,
+    'title': 'Paid electricity bill',
+    'category': 'Bills',
+    'amount': 128000,
+  },
+  {
+    'id': 3,
+    'title': 'Paid course subscription',
+    'category': 'Subscriptions',
+    'amount': 1800000,
+  },
+  {
+    'id': 4,
+    'title': 'Bought groceries',
+    'category': 'Groceries',
+    'amount': 225000,
+  },
+  {
+    'id': 5,
+    'title': 'Paid credit debt',
+    'category': 'Debts',
+    'amount': 300000,
+  },
+  {
+    'id': 6,
+    'title': 'Bought keyboard',
+    'category': 'Electronics',
+    'amount': 778000,
+  },
+];
