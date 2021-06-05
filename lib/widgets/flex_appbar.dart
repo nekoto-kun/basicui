25249499e5acf408c1c4a4aa773e8621b88f85c9
Andre Kho <andreyonathan65@gmail.com>
2021-06-05 10:57:33 +0700

    Update flex AppBar

--- lib/widgets/flex_appbar.dart
+++ lib/widgets/flex_appbar.dart
@@ -8,12 +8,16 @@ class FlexAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverAppBar(
       expandedHeight: 100,
+      collapsedHeight: 84,
       floating: false,
       pinned: true,
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       elevation: 0,
       flexibleSpace: FlexibleSpaceBar(
-        titlePadding: EdgeInsetsDirectional.only(start: 16, bottom: 16),
+        titlePadding: EdgeInsetsDirectional.only(
+          start: 16,
+          bottom: 16,
+        ),
         title: Text(
           '$title',
           style: TextStyle(
