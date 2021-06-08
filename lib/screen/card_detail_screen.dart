4abe85159b9b623a082bcef783f91fd21f7455a3
Andre Kho <andreyonathan65@gmail.com>
2021-06-08 16:03:45 +0700

    Refine interface and add three buttons

--- lib/screen/card_detail_screen.dart
+++ lib/screen/card_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:basicui/model/card.dart';
+import 'package:basicui/screen/home_screen.dart';
 import 'package:basicui/util/misc.dart';
 import 'package:basicui/util/money.dart';
 import 'package:basicui/widgets/nav_button_widget.dart';
@@ -35,7 +36,7 @@ class _CardDetailScreenState extends State<CardDetailScreen> {
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         elevation: 0,
         automaticallyImplyLeading: false,
-        toolbarHeight: 84,
+        toolbarHeight: 100,
         title: Text(
           '${widget.card.name}',
           style: TextStyle(fontWeight: FontWeight.w700),
@@ -45,17 +46,17 @@ class _CardDetailScreenState extends State<CardDetailScreen> {
           displayShadow: true,
           child: Icon(FeatherIcons.arrowLeft),
           color: Theme.of(context).scaffoldBackgroundColor,
-          margin: EdgeInsets.all(16),
+          margin: EdgeInsets.all(24),
         ),
-        leadingWidth: 84,
+        leadingWidth: 100,
         actions: [
           SizedBox(
-            width: 84,
+            width: 100,
             child: NavigationButton(
               displayShadow: true,
               child: Icon(FeatherIcons.settings),
               color: Theme.of(context).scaffoldBackgroundColor,
-              margin: EdgeInsets.all(16),
+              margin: EdgeInsets.all(24),
             ),
           ),
         ],
@@ -65,34 +66,61 @@ class _CardDetailScreenState extends State<CardDetailScreen> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            SizedBox(
+            // Details and Card
+            Container(
               height: MediaQuery.of(context).size.height / 4,
+              margin: EdgeInsets.symmetric(vertical: 8),
               child: Row(
                 children: [
+                  // Details
                   Expanded(
                     flex: 3,
-                    child: Container(
-                      padding: EdgeInsets.all(16),
+                    child: Padding(
+                      padding: EdgeInsets.all(24),
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
+                          Spacer(),
                           Expanded(
-                            child: AutoSizeText(
-                              '${widget.card.name}',
+                            child: Align(
+                              alignment: Alignment.bottomLeft,
+                              child: AutoSizeText(
+                                '${widget.card.name}',
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                maxFontSize: 24,
+                                minFontSize: 18,
+                                maxLines: 1,
+                              ),
                             ),
                           ),
                           Expanded(
+                            flex: 2,
                             child: AutoSizeText(
                               '${Money.fromInt(widget.card.balance, idr)}',
-                              style: TextStyle(fontWeight: FontWeight.w700),
-                              maxFontSize: 48,
-                              minFontSize: 18,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 36,
+                              ),
+                              maxFontSize: 36,
+                              minFontSize: 24,
                               maxLines: 1,
                             ),
                           ),
+                          Spacer(),
                           Expanded(
+                            flex: 2,
                             child: AutoSizeText(
                               'For the current billing period, you will earn ${Money.fromInt(widget.card.balance * 5 ~/ 1000, idr)} cashback!',
+                              style: TextStyle(
+                                color: Colors.black45,
+                                fontSize: 14,
+                              ),
+                              maxFontSize: 14,
+                              minFontSize: 8,
+                              maxLines: 2,
                             ),
                           ),
                           Expanded(
@@ -100,31 +128,37 @@ class _CardDetailScreenState extends State<CardDetailScreen> {
                               children: [
                                 Icon(
                                   FeatherIcons.arrowUpCircle,
-                                  color: Colors.green,
+                                  color: Colors.greenAccent[400],
                                 ),
-                                SizedBox(width: 8),
+                                SizedBox(width: 12),
                                 Expanded(
                                   child: AutoSizeText(
                                     '${Money.fromInt(widget.card.balance * 5 ~/ 1000, idr)}',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                    ),
                                   ),
                                 ),
                               ],
                             ),
                           ),
+                          Spacer(),
                         ],
                       ),
                     ),
                   ),
+                  // Card
                   Flexible(
                     child: Builder(
                       builder: (_) {
                         String? _imagePath =
                             cardLogoPath('${widget.card.type}');
                         return Container(
+                          height: 256,
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(16),
-                              bottomLeft: Radius.circular(16),
+                              topLeft: Radius.circular(24),
+                              bottomLeft: Radius.circular(24),
                             ),
                             color: cardColor('${widget.card.color}'),
                           ),
@@ -138,15 +172,19 @@ class _CardDetailScreenState extends State<CardDetailScreen> {
                                   image: AssetImage(_imagePath),
                                   height: 60,
                                 ),
+                              Spacer(flex: 4),
                               Flexible(
                                 child: Row(
                                   children: [
                                     Expanded(
                                       child: AutoSizeText(
-                                        '${widget.card.cardNumber}',
+                                        '${widget.card.cardNumber.substring(widget.card.cardNumber.length - 9).replaceRange(0, 4, '****')}',
                                         style: cardTextStyle(
                                                 '${widget.card.color}')
                                             .copyWith(fontFamily: 'Fira Code'),
+                                        maxFontSize: 16,
+                                        minFontSize: 6,
+                                        maxLines: 1,
                                       ),
                                     ),
                                   ],
@@ -161,8 +199,75 @@ class _CardDetailScreenState extends State<CardDetailScreen> {
                 ],
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(24),
+              child: GridView(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 16,
+                  childAspectRatio: 6 / 5,
+                ),
+                shrinkWrap: true,
+                children: [
+                  CardButton(
+                    icon: Icon(FeatherIcons.lock),
+                    text: 'Block',
+                  ),
+                  CardButton(
+                    icon: Icon(FeatherIcons.arrowUpCircle),
+                    text: 'Pay',
+                  ),
+                  CardButton(
+                    icon: Icon(FeatherIcons.plusCircle),
+                    text: 'Top Up',
+                  ),
+                ],
+              ),
+            ),
+            HomeContainer(
+              title: 'Next write-offs',
+              child: Text('Horizontal view here'),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CardButton extends StatelessWidget {
+  final Icon icon;
+  final String text;
+  final Color color;
+
+  const CardButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+    this.color = const Color(0x0C000000),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+        color: color,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          icon,
+          AutoSizeText(
+            text,
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
       ),
     );
   }
